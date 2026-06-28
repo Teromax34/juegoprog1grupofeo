8671c9c7156e447e535019c2610f596299afe726
Juan <juancinta2@gmail.com>
2026-06-28 20:55:14 -0300

    juan-cinta

--- juan.py
+++ juan.py
@@ -1,50 +1,36 @@
 def peliculas():
     cont = 0
     cont_error = 0
-    print("Pregunta 1")
-    opcion = input("Un hombre azul llega a un planeta llamado Pandora: \n1-Avatar\n2-Titanic\n3-Avengers\n")
-    if opcion == "1":
-        print("Correcto")
-        cont +=1
-    else:
-        print("Incorrecto")
-        cont_error += 1
-    print("Pregunta 2")
-    opcion = input("Un barco choca contra un iceberg: \n1-Frozen \n2-Avatar \n3-Titanic\n")
-    if opcion == "3":
-        print("Correcto")
-        cont +=1
-    else:
-        print("Incorrecto")
-        cont_error += 1
-    print("Pregunta 3")
-    opcion = input("Un grupo de superhéroes se une para derrotar a Thanos: \n1- Batman \n2-Avengers Endgame \n3-Toy Story\n")
-    if opcion == "2":
-        print("Correcto")
-        cont +=1
-    else:
-        print("Incorrecto")
-        cont_error += 1
-    print("Pregunta 4")
-    opcion = input("Un pez payaso busca a su hijo perdido en el océano: \n1-Buscando a Nemo \n2-Cars \n3-Ratatouille\n")
-    if opcion == "1":
-        print("Correcto")
-        cont +=1
-    else:
-        print("Incorrecto")
-        cont_error += 1
-    print("Pregunta 5")
-    opcion = input("Un chef es en realidad una rata que cocina en París: \n1-Ratatouille \n2-Coco \n3-Intensamente\n")
-    if opcion == "1":
-        print("Correcto")
-        cont +=1
-    else:
-        print("Incorrecto")
-        cont_error += 1
-        
+    preguntas = [
+        {"pregunta": "Un hombre azul llega a un planeta llamado Pandora:",
+         "opciones": ["Avatar", "Titanic", "Avengers"],
+         "correcta": 1},
+        {"pregunta": "Un barco choca contra un iceberg:",
+         "opciones": ["Frozen", "Avatar", "Titanic"],
+         "correcta": 3},
+        {"pregunta": "Un grupo de superhéroes se une para derrotar a Thanos:",
+         "opciones": ["Batman", "Avengers Endgame", "Toy Story"],
+         "correcta": 2},
+        {"pregunta": "Un pez payaso busca a su hijo perdido en el océano:",
+         "opciones": ["Buscando a Nemo", "Cars", "Ratatouille"],
+         "correcta": 1},
+        {"pregunta": "Un chef es en realidad una rata que cocina en París:",
+         "opciones": ["Ratatouille", "Coco", "Intensamente"],
+         "correcta": 1},
+    ]
+    for i in range(len(preguntas)):
+        print(preguntas[i]["pregunta"])
+        for j in range(len(preguntas[i]["opciones"])):
+            print(f"{j+1} , {preguntas[i]["opciones"][j]}")
+            
+        respuestas = int(input("Ingresar la Respuesta: "))
+        if respuestas == preguntas[i]["correcta"]:
+            cont +=1
+        else:
+            cont_error +=1
+
     porc = (cont * 100) / (cont + cont_error)
     print(f"Los aciertos que tuviste fueron: {cont}")
     print(f"Los errores fueron: {cont_error}")
     print(f"El porcentaje de acierto es de: {porc}%")
-    
 peliculas()
